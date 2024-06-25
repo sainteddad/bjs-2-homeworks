1e3496000d4bdce849626b95d64d7ef32e57c878
memphis <pizdajerafa123@gmail.com>
2024-06-25 02:57:48 -0700

    первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,31 +3,22 @@
 function solveEquation(a, b, c) {
 	let arr = [];
 	let d = b ** 2 - 4 * a * c;
-	if (d < 0) {
-		return arr;
-	} else if (d === 0) {
-		arr = [-b / (2 * a)];
-	} else if (d > 0) {
-		arr = [(-b + Math.sqrt(b) / (2 * a))];
-	} else if (d > 0) {
-		arr = [(-b - Math.sqrt(b) / (2 * a))];
+	length = Math.sqrt(d);
+	if (d < 0) return false; 
+	if (d > 0) return true;
+	if (d === 0) {
+		arr = [a, b, c];
 	}
 }
 
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let p = percent / 100 / 12;
-	let s = amount - contribution;
-	let monthlyPayment = s * (p + (p / (((1 + p))) ** countMonths - 1));
-	let totalAmount = monthlyPayment * countMonths;
-	return +totalAmount.toFixed(2);
+	let monthPercent = (percent / 100) / 12;
+	let creditBody = amount - contribution;
+	let monthlyPayment = creditBody * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+	let totalAmount = percentages((monthlyPayment * countMonths).toFixed(2));
+	return +totalAmount;
 }
 
-100, 0, 50000, 12
-10, 1000, 50000,12
-10, 0, 20000, 24
-10, 1000, 20000, 24
-10, 20000, 20000, 24
-10, 0, 10000, 36
-15, 0, 10000, 36
+
