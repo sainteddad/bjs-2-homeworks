9c00890896c571ecad0f148cefdd9e72442d14c4
memphis <pizdajerafa123@gmail.com>
2024-07-04 16:55:23 -0700

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,25 +2,23 @@
 function solveEquation(a, b, c) {
 	let arr = [];
 	let d = b ** 2 - 4 * a * c;
-	if (d < 0) {
-	  arr = [];
-	} else if (d === 0) {
-	  arr = [-b / (2 * a)];
-	} else {
-	  arr = [
-		(-b + Math.sqrt(d)) / (2 * a),
-		(-b - Math.sqrt(d)) / (2 * a)
-	  ];
-	}
+	if (d === 0) {
+		arr = [-b / (2 * a)];
+	} else if (d > 0) {
+		arr = [
+			(-b + Math.sqrt(d)) / (2 * a),
+			(-b - Math.sqrt(d)) / (2 * a)
+		  ];
+	} 
+
 	return arr;
   }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let monthPercent = (precent / 100) / 12;
+	let monthPercent = (percent / 100) / 12;
 	let creditBody = amount - contribution;
-	let monthlyPay = creditBody * (monthPercent + (monthPercent / (1 + monthPercent) ** countMonths) - 1 );
-	let totalPayment = parseFloat((monthlyPay * countMonths).toFixed(2));
-	return totalPayment;
+	let monthlyPay = creditBody * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1 )));
+	return parseFloat((monthlyPay * countMonths).toFixed(2));
 }
 
 
