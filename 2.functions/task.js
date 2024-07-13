fa10aa231a9358be6a069ab4ff7c9af1ed7c9521
memphis <pizdajerafa123@gmail.com>
2024-07-12 21:59:40 -0700

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,19 +1,42 @@
 function getArrayParams(...arr) {
+  let min = -infinity;
+  let max = infinity;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    } 
+  }
+  
+  getArrayParams(-59, 59, 20)
+  getArrayParams(1, 2, 3, -60, 20)
+  getArrayParams(5)
 
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
+  
+  
+  
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  } 
 
-}
 
 function differenceMaxMinWorker(...arr) {
-
+  
 }
 
 function differenceEvenOddWorker(...arr) {
+  
+  }
 
-}
 
 function averageEvenElementsWorker(...arr) {
 
