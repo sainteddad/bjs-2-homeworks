a503accfbb80c0cc77ba25fb8901c76164b68270
memphis <pizdajerafa123@gmail.com>
2024-07-21 21:15:28 -0700

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,17 +1,22 @@
 function getArrayParams(...arr) {
-  let min = -min;
-  let max = max;
+  let min = -Infinity;
+  let max = Infinity;
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    } else if (arr[i] < min) {
-      min = arr[i];
-    } 
+     sum += arr[i];
+  }
+
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+
+  if (arr[i] < min) {
+    min = arr[i];
   }
 
   return { min: min, max: max, avg: avg };
 }
+
 
 function summElementsWorker(...arr) {
   
@@ -33,11 +38,13 @@ function differenceEvenOddWorker(...arr) {
   let sumEvenElement = 0;
   let sumOddElement = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      sumEvenElement += arr[i];
-    } else sumOddElement += arr[i];
-     
-  }
+    if (arr [i] % 2 === 0) sumEvenElement += arr [i]; {
+      sumOddElement += arr [i];
+    } 
+    if (arr [i] % 2 === 1) sumOddElement += arr [i]; {
+      sumEvenElement += arr [i];
+    }
+  } 
   }
 
 
@@ -45,13 +52,15 @@ function averageEvenElementsWorker(...arr) {
   let sumEvenElement = 0;
   let countEvenElement = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      sumEvenElement += arr[i];
+    if (arr [i] % 2 === 0) sumEvenElement += arr [i]; {
+      countEvenElement++;
+    }
+    if (arr [i] % 2 === 1) sumEvenElement += arr [i]; {
       countEvenElement++;
     }
   }
 }
 
 function makeWork (arrOfArr, func) {
-  
+
 }
