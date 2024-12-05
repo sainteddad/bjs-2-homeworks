8c66d9d4d9b052297600a29f2477c67a70a2e475
memphis <pizdajerafa123@gmail.com>
2024-12-04 18:35:07 -0800

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,9 +5,12 @@ function Student(name, gender, age) {
 }
 
 let student1 = new Student ("Antony", "Male", 21);
-
+student1.subjectName("Algebra");
+student1.marks(...4, 5, 4, 5);
 let student2 = new Student ("Rose", "Female", 20);
-
+student2.subjectName("Geometry");
+student2.marks();
+student2.exclude('плохая учёба')
 
 
 Student.prototype.setSubject = function (subjectName) {
