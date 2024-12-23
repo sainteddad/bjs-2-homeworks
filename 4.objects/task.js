86f87e00bac8f07902279a3f69a9848b1b667243
memphis <pizdajerafa123@gmail.com>
2024-12-23 05:07:52 -0800

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -12,15 +12,21 @@ let student1 = new Student ("Antony", "Male", 21);
     
 let student2 = new Student ("Rose", "Female", 20);
 
+
+
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
     
 }
 
 Student.prototype.addMarks = function (...marks) {
-    [this.marks = 5, ...marks]; 
+    this.marks = marks[[5], [4, 4], [3, 3, 4, 5]];
+    marks[0];
+    marks[1];
+    marks[3];
     
-
+    
+    
     
     
     
